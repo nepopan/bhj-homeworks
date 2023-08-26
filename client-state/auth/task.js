e65af8e69ab69a1dc226437bf921a57ea5df7f13
Pavel Tarazevich <pashatarazevich@gmail.com>
2023-08-26 20:15:06 +0300

    fix commit auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -2,15 +2,20 @@ const signinBlock = document.getElementById("signin");
 const welcomeBlock = document.getElementById("welcome");
 const userIdSpan = document.getElementById("user_id");
 
-const userId = localStorage.getItem("user_id");
+function updateVisibility() {
+    const userId = localStorage.getItem("user_id");
 
-if (userId) {
-    welcomeBlock.classList.add("welcome_active");
-    userIdSpan.textContent = userId;
-} else {
-    signinBlock.classList.add("signin_active");
+    if (userId) {
+        welcomeBlock.classList.add("welcome_active");
+        signinBlock.classList.remove("signin_active");
+        userIdSpan.textContent = userId;
+    } else {
+        welcomeBlock.classList.remove("welcome_active");
+        signinBlock.classList.add("signin_active");
+    }
 }
 
+updateVisibility();
 
 const signinForm = document.getElementById("signin__form");
 const signinBtn = document.getElementById("signin__btn");
@@ -28,9 +33,7 @@ signinBtn.addEventListener("click", function(event) {
     .then(data => {
         if (data.success) {
             localStorage.setItem("user_id", data.user_id);
-            signinBlock.classList.remove("signin_active");
-            welcomeBlock.classList.add("welcome_active");
-            userIdSpan.textContent = data.user_id;
+            updateVisibility();
         } else {
             alert("Неверный логин/пароль");
         }
